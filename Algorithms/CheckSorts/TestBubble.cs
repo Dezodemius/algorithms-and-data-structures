2c687c20c0f844c51d6c55ae6329b02fbf6ca8b3
Гладков Егор (Gladkov_ES) <Gladkov_ES@directum.ru>
2020-02-17 13:27:33 +0400

    Немного исправил тесты.
    - В обычную сортировку пузырьком добавил цикл, который прогоняет сортировку на несколько раз.
    - Добавил новые регионы.
    - Немного рефакторинга.

--- Algorithms/CheckSorts/TestBubble.cs
+++ Algorithms/CheckSorts/TestBubble.cs
@@ -10,36 +10,108 @@ namespace CheckSorts
     public class TestBubble
     {
         #region Поля
-
+        
+        /// <summary>
+        /// Размер тестируемого набора данных.
+        /// </summary>
         private const int Size = 1000;
         
+        /// <summary>
+        /// Количество прогонов сортировок.
+        /// </summary>
+        private const int NumberOfMeasurements = 100;
+        
+        /// <summary>
+        /// Объект рандомайзера.
+        /// </summary>
         private static readonly Random Rand = new Random();
 
         #endregion
 
         #region Тесты
 
-        [Test]
+        #region Простой пузырёк.
+
+         [Test]
         public void TestSimple()
         {
             var stopwatch = new Stopwatch();
             var list = new List<int>();
+            var sortingTime = new TimeSpan();
+
+            for (var i = 0; i < NumberOfMeasurements; i++)
+            {
+                list = new List<int>();
+                for (var j = 0; j < Size; j++)
+                    list.Add(Rand.Next());
+                
+                stopwatch.Start();
+                Bubble<int>.Simple(ref list);
+                stopwatch.Stop();
+                
+                sortingTime += stopwatch.Elapsed;
+            }
             
-            for (int i = 0; i < Size; i++)
-                list.Add(Rand.Next());
-            
-            stopwatch.Start();
             var sortedList = list.OrderBy(i => i);
-            stopwatch.Stop();
-            Console.WriteLine($"Встроенная сортировка: {stopwatch.Elapsed}");
-            
-            stopwatch.Start();
-            Bubble<int>.Simple(ref list);
-            stopwatch.Stop();
-            Console.WriteLine($"Обыкновенная сортировка пузырьком: {stopwatch.Elapsed}");
-            
+            Console.WriteLine($"Обыкновенная сортировка пузырьком: {sortingTime / NumberOfMeasurements}");
             Assert.IsTrue(sortedList.SequenceEqual(list));
         }
+        
+        [Test]
+        public void TestSimpleOnReverseList()
+        {
+            var stopwatch = new Stopwatch();
+            var list = new List<int>();
+            var sortingTime = new TimeSpan();
+
+            for (var i = 0; i < NumberOfMeasurements; i++)
+            {
+                list = new List<int>();
+                for (var j = 0; j < Size; j++)
+                    list.Add(Rand.Next());
+                
+                list = list.OrderByDescending(e => e).ToList();
+                
+                stopwatch.Start();
+                Bubble<int>.Simple(ref list);
+                stopwatch.Stop();
+                sortingTime += stopwatch.Elapsed;
+            }
+            
+            var sortedList = list.OrderBy(i => i);
+            Console.WriteLine($"Обыкновенная сортировка пузырьком: {sortingTime / NumberOfMeasurements}");
+            Assert.IsTrue(sortedList.SequenceEqual(list));
+        }
+        
+        [Test]
+        public void TestSimpleOnSortedList()
+        {
+            var stopwatch = new Stopwatch();
+            var list = new List<int>();
+            var sortingTime = new TimeSpan();
+
+            for (var i = 0; i < NumberOfMeasurements; i++)
+            {
+                list = new List<int>();
+                for (var j = 0; j < Size; j++)
+                    list.Add(Rand.Next());
+                
+                list = list.OrderBy(e => e).ToList();
+                
+                stopwatch.Start();
+                Bubble<int>.Simple(ref list);
+                stopwatch.Stop();
+                sortingTime += stopwatch.Elapsed;
+            }
+            
+            var sortedList = list.OrderBy(i => i);
+            Console.WriteLine($"Обыкновенная сортировка пузырьком: {sortingTime / NumberOfMeasurements}");
+            Assert.IsTrue(sortedList.SequenceEqual(list));
+        }
+
+        #endregion
+
+        #region Улучшенный пузырёк.
         
         [Test]
         public void TestAdvanced()
@@ -47,7 +119,7 @@ namespace CheckSorts
             var stopwatch = new Stopwatch();
             var list = new List<int>();
             
-            for (int i = 0; i < Size; i++)
+            for (var i = 0; i < Size; i++)
                 list.Add(Rand.Next());
             
             stopwatch.Start();
@@ -64,12 +136,64 @@ namespace CheckSorts
         }
         
         [Test]
+        public void TestAdvancedOnReverseList()
+        {
+            var stopwatch = new Stopwatch();
+            var list = new List<int>();
+            
+            for (var i = 0; i < Size; i++)
+                list.Add(Rand.Next());
+            
+            list = list.OrderByDescending(a => a).ToList();
+            
+            stopwatch.Start();
+            var sortedList = list.OrderBy(i => i);
+            stopwatch.Stop();
+            Console.WriteLine($"Встроенная сортировка: {stopwatch.Elapsed}");
+            
+            stopwatch.Start();
+            Bubble<int>.Advanced(ref list);
+            stopwatch.Stop();
+            Console.WriteLine($"Улучшенная сортировка пузырьком: {stopwatch.Elapsed}");
+            
+            Assert.IsTrue(sortedList.SequenceEqual(list));
+        }
+        
+        [Test]
+        public void TestAdvancedOnSortedList()
+        {
+            var stopwatch = new Stopwatch();
+            var list = new List<int>();
+            
+            for (var i = 0; i < Size; i++)
+                list.Add(Rand.Next());
+            
+            list = list.OrderBy(i => i).ToList();
+            
+            stopwatch.Start();
+            var sortedList = list.OrderBy(i => i);
+            stopwatch.Stop();
+            Console.WriteLine($"Встроенная сортировка: {stopwatch.Elapsed}");
+            
+            stopwatch.Start();
+            Bubble<int>.Advanced(ref list);
+            stopwatch.Stop();
+            Console.WriteLine($"Улучшенная сортировка пузырьком: {stopwatch.Elapsed}");
+            
+            Assert.IsTrue(sortedList.SequenceEqual(list));
+        }
+
+        #endregion
+
+        #region Экспериментальный пузырёк.
+
+        [Test]
         public void TestExperimental()
         {
             var stopwatch = new Stopwatch();
             var list = new List<double>();
             
-            for (int i = 0; i < Size; i++)
+            for (var i = 0; i < Size; i++)
                 list.Add(Rand.NextDouble());
             
             stopwatch.Start();
@@ -80,10 +204,59 @@ namespace CheckSorts
             stopwatch.Start();
             Bubble<int>.Experimental(ref list);
             stopwatch.Stop();
-            Console.WriteLine($"Улучшенная сортировка пузырьком: {stopwatch.Elapsed}");
+            Console.WriteLine($"Эксперименталньая сортировка пузырьком: {stopwatch.Elapsed}");
             
             Assert.IsTrue(sortedList.SequenceEqual(list));
         }
+        
+        [Test]
+        public void TestExperimentalOnReverseList()
+        {
+            var stopwatch = new Stopwatch();
+            var list = new List<double>();
+            
+            for (var i = 0; i < Size; i++)
+                list.Add(Rand.NextDouble());
+            
+            list = list.OrderByDescending(a => a).ToList();
+            stopwatch.Start();
+            var sortedList = list.OrderBy(i => i);
+            stopwatch.Stop();
+            Console.WriteLine($"Встроенная сортировка: {stopwatch.Elapsed}");
+            
+            stopwatch.Start();
+            Bubble<int>.Experimental(ref list);
+            stopwatch.Stop();
+            Console.WriteLine($"Эксперименталньая сортировка пузырьком: {stopwatch.Elapsed}");
+            
+            Assert.IsTrue(sortedList.SequenceEqual(list));
+        }
+        
+        [Test]
+        public void TestExperimentalOnSortedList()
+        {
+            var stopwatch = new Stopwatch();
+            var list = new List<double>();
+            
+            for (var i = 0; i < Size; i++)
+                list.Add(Rand.NextDouble());
+            
+            list = list.OrderBy(a => a).ToList();
+            
+            stopwatch.Start();
+            var sortedList = list.OrderBy(i => i);
+            stopwatch.Stop();
+            Console.WriteLine($"Встроенная сортировка: {stopwatch.Elapsed}");
+            
+            stopwatch.Start();
+            Bubble<int>.Experimental(ref list);
+            stopwatch.Stop();
+            Console.WriteLine($"Эксперименталньая сортировка пузырьком: {stopwatch.Elapsed}");
+            
+            Assert.IsTrue(sortedList.SequenceEqual(list));
+        }
+        
+        #endregion
         
         #endregion
     }
